022ada8b9653193011feb4de00c9926ad72b7657
NewbsterPolez <70821919+NewbsterPolez@users.noreply.github.com>
2020-09-13 12:58:40 -0400

    Add files via upload

---
+++ NPC Creator/CustomOdinAttributes/ColorBoxGroup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using System;
+
+public class ColorBoxGroup : PropertyGroupAttribute
+{
+    public float R =1 , G = 1, B = 1, A = 1;
+
+    public bool ShowLabel;
+
+    public ColorBoxGroup(string path) : base(path)
+    {
+
+    }
+
+    public ColorBoxGroup(string path, float r, float g, float b, float a = 1f, bool showLabel = true) : base(path)
+    {
+        this.R = r;
+        this.G = g;
+        this.B = b;
+        this.A = a;
+        this.ShowLabel = showLabel;
+    }
+
+    public class ColorBoxGroupDrawer : OdinGroupDrawer<ColorBoxGroup>
+    {
+        protected override void DrawPropertyLayout(GUIContent label)
+        {
+            GUIHelper.PushColor(new Color(this.Attribute.R, this.Attribute.G, this.Attribute.B, this.Attribute.A));
+            SirenixEditorGUI.BeginBox();
+            if (this.Attribute.ShowLabel == true)
+            {
+                SirenixEditorGUI.BeginBoxHeader();
+                GUIHelper.PopColor();
+                GUILayout.Label(label);
+                SirenixEditorGUI.EndBoxHeader();
+            }
+            else 
+                GUIHelper.PopColor();
+
+            for (int i = 0; i < this.Property.Children.Count; i++)
+            {
+                this.Property.Children[i].Draw();
+            }
+            
+            SirenixEditorGUI.EndBox();
+        }
+    }
+}
---
+++ NPC Creator/CustomOdinAttributes/ColorFoldoutGroup.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+
+public class ColorFoldoutGroup : PropertyGroupAttribute
+{
+    public float R =1 , G = 1, B = 1, A = 1;
+    public bool Expanded;
+
+    public ColorFoldoutGroup(string path) : base (path)
+    {
+
+    }
+
+    public ColorFoldoutGroup(string path, float r, float g, float b, float a = 1f, bool expanded = false) : base(path)
+    {
+        this.R = r;
+        this.G = g;
+        this.B = b;
+        this.A = a;
+        this.Expanded = expanded;
+    }
+}
+
+public class ColorFoldoutGroupDrawer: OdinGroupDrawer<ColorFoldoutGroup>
+{
+    private bool isOpen;
+
+    protected override void Initialize()
+    {
+        isOpen = this.Attribute.Expanded;
+    }
+    protected override void DrawPropertyLayout(GUIContent label)
+    {
+        GUIHelper.PushColor(new Color(this.Attribute.R, this.Attribute.G, this.Attribute.B, this.Attribute.A));
+        SirenixEditorGUI.BeginBox();
+        SirenixEditorGUI.BeginBoxHeader();
+        GUIHelper.PopColor();
+
+        this.isOpen = SirenixEditorGUI.Foldout(this.isOpen, label);
+        SirenixEditorGUI.EndBoxHeader();
+
+        if(SirenixEditorGUI.BeginFadeGroup(this, isOpen))
+        {
+            for(int i = 0; i < this.Property.Children.Count; i++)
+            {
+                this.Property.Children[i].Draw();
+            }
+        }
+        SirenixEditorGUI.EndFadeGroup();
+        SirenixEditorGUI.EndBox();
+    }
+}
